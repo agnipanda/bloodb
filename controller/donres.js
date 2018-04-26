1073a5ad07b75cdc1d4ad8033c970747f81c3408
ganeshpatro321 <ganeshpatro321@gmail.com>
2018-04-26 14:23:35 +0530

    mailing for donation

--- controller/donres.js
+++ controller/donres.js
@@ -1,11 +1,16 @@
 var mongoose = require('mongoose');
 var donModel = require('../models/donors');
+const sgMail = require('@sendgrid/mail');
+var dotenv = require('dotenv');
+dotenv.config();
+
 
 var donUser = function(req,res){
   var donmodel = new donModel({
     fname : req.body.fname,
     lname : req.body.lname,
     age : req.body.age,
+    email : req.body.email,
     place : req.body.place,
     gender : req.body.gender,
     bgroup : req.body.bgroup
@@ -17,5 +22,22 @@ var donUser = function(req,res){
       res.render('./pages/success');
     };
   });
-}
+  sgMail.setApiKey(process.env.API_KEY);
+    const msg = {
+        to: "bloodb@gmail.com",
+        from: req.body.email,
+        subject:"("+req.body.name+")"+"Donation Response",
+        text: "Person Stays in" + req.body.place + ", BloodGroup: "+ req.body.bgroup,
+    };
+    sgMail.send(msg);
+    const msg2 = {
+        to: req.body.email,
+        from: "bloodb@gmail.com",
+        subject: "Thank You",
+        text:"Hello,"+req.body.fname+" " + req.body.lname +" . Thanks for saving a life!, We will contact you soon with further details about donation time and date",
+    };
+    sgMail.send(msg2);
+    };
+
+
 module.exports = {"donUser" : donUser };
--- models/donors.js
+++ models/donors.js
@@ -5,6 +5,7 @@ var donSchema = new Schema({
   fname : { type : String, required : true },
   lname : { type : String, required : true },
   age : { type : Number, required : true, min:18, max:60 },
+  email : { type : String, required : true},
   place : { type : String, required : true },
   gender : { type : String, required : true, min:1 },
   bgroup : { type : String, required : true, max:3 }
